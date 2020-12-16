bce31fc720022dcccf5a81af26656fd4c91fdf6e
Abir Mahmud <amabirmahmud@gmail.com>
2020-12-17 05:10:25 +0600

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Role-Based-Authorization
+
+https://www.c-sharpcorner.com/article/role-based-authentication-in-asp-net-mvc/
---
+++ TestingRole/TestingRole/Controllers/HomeController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using TestingRole.Models;
+
+namespace TestingRole.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        private readonly EmployeeContext _dbContext = new EmployeeContext();
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(User user)
+        {
+            if (ModelState.IsValid)
+            {
+                bool IsValidUser = _dbContext.Users
+               .Any(u => u.Username.ToLower() == user
+               .Username.ToLower() && user
+               .Password == user.Password);
+
+                if (IsValidUser)
+                {
+                    FormsAuthentication.SetAuthCookie(user.Username, false);
+                    return RedirectToAction("Index", "Employees");
+                }
+            }
+            ModelState.AddModelError("", "invalid Username or Password");
+            return View();
+        }
+        public ActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Register(User registerUser)
+        {
+            if (ModelState.IsValid)
+            {
+                _dbContext.Users.Add(registerUser);
+                _dbContext.SaveChanges();
+                return RedirectToAction("Login");
+
+            }
+            return View();
+        }
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Login");
+        }
+    }
+}
